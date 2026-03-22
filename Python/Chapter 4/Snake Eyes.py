eacfba03c580fa634e825c4fe27b1f981b69faaa
Duncan <205854574+AmpVolume@users.noreply.github.com>
2026-03-22 12:05:13 -0400

    Create Snake Eyes.py

---
+++ Python/Chapter 4/Snake Eyes.py
@@ -0,0 +1,16 @@
+import random
+
+# First roll
+die1 = random.randint(1, 6)
+die2 = random.randint(1, 6)
+total = die1 + die2
+
+# Keep rolling until total is 2 (snake eyes)
+while total != 2:
+    print("Nope")
+    
+    die1 = random.randint(1, 6)
+    die2 = random.randint(1, 6)
+    total = die1 + die2
+
+print("Snake eyes!")
